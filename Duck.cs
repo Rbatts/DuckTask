99fe5e2395bfff78a55a5159c658731189c0138a
Rbatts <32482377+Rbatts@users.noreply.github.com>
2018-03-02 09:11:30 +0000

    Duck Task Completed

---
+++ Duck.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckTask
+{
+    public class Duck : IComparable<Duck>
+    {
+        public string Name { get; }
+        public string Type { get; }
+        public int WeightInGrams { get; }
+        public int AgeInMonths { get; }
+
+        public Duck(string name, string type, int weightInGrams, int ageInMonths)
+        {
+            Name = name;
+            Type = type;
+            WeightInGrams = weightInGrams;
+            AgeInMonths = ageInMonths;
+        }
+
+        public override bool Equals(object obj)
+        {
+            Duck duck = obj as Duck;
+            if (duck == null)
+            {
+                return false;
+            }
+
+            return Name == duck.Name && 
+                   Type == duck.Type && 
+                   WeightInGrams == duck.WeightInGrams && 
+                   AgeInMonths == duck.AgeInMonths;
+        }
+
+        public override string ToString()
+        {
+            return string.Format("\n\n Name : {0}," +
+                                 "\n\n Type: {1}," +
+                                 "\n\n Weight : {2} Grams," +
+                                 "\n\n Age : {3} Months",
+                                 Name, Type, WeightInGrams, AgeInMonths);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                int hash = 31;
+                hash = hash * 23 + Name.GetHashCode();
+                hash = hash * 23 + Type.GetHashCode();
+                hash = hash * 23 + WeightInGrams.GetHashCode();
+                hash = hash * 23 + AgeInMonths.GetHashCode();
+                return hash;
+            }
+        }
+
+        public int CompareTo(Duck otherDuck)
+        {
+            if (AgeInMonths < otherDuck.AgeInMonths)
+            {
+                return 1;
+            }
+            else if (AgeInMonths > otherDuck.AgeInMonths)
+            {
+                return -1;
+            }
+            else
+            {
+                return 0;
+            }
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DuckTask
+{
+    public class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Dictionary<Duck, string> duckDictionary = new Dictionary<Duck, string>
+            {
+                {new Duck("Sir Quacksalot", "Mallard", 100, 17), ""},
+                {new Duck("Quackers", "Legend", 120, 20), ""},
+                {new Duck("Lenny", "Flappy", 80, 10), ""},
+                {new Duck("Rodger", "Old", 110, 25), ""},
+                {new Duck("Harold", "Fancy", 200, 23), ""},
+                {new Duck("Bread Boy", "Mallard", 190, 30), ""}
+            };
+
+            List<Duck> duckList = duckDictionary.OrderByDescending(d => d.Value).Select(d => d.Key).ToList();
+
+            foreach (var duck in duckList)
+            {
+                Console.WriteLine(duck);
+                Console.ReadLine();
+            }
+        }
+    }
+
+}
